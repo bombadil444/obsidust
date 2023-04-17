3a315dc3e6ce407ffd97bee7875ac4339b2c6451
Bombadil <mastwyk42@gmail.com>
2023-04-16 22:00:07 -0500

    add command line args

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
+use clap::Parser;
 use std::fs::{create_dir, rename};
 use std::io;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
-
 // fn print_type_of<T>(_: &T) {
 //     println!("{}", std::any::type_name::<T>())
 // }
 
+/// Command line tools for Obsidian
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// prefix of files to move
+    #[arg(short, long)]
+    prefix: String,
+}
+
 fn main() -> io::Result<()> {
+    let args: Args = Args::parse();
+
     // TODO: make dynamic
     let user_path: &str = "/home/bombadil";
     let notes_path: &str = "vault/daily notes";
-    let prefix: &str = "2023-03";
 
+    move_files(user_path, notes_path, &args.prefix)?;
+
+    Ok(())
+}
+
+fn move_files(user_path: &str, notes_path: &str, prefix: &str) -> io::Result<()> {
     let vault_dir: PathBuf = Path::new(user_path).join(notes_path);
     let dest_dir: PathBuf = vault_dir.join(prefix);
 
